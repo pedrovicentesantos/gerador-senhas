92b56c54389c7d998784d563c5a2cc17dcb379c1
Pedro Vicente <pedrovicente@poli.ufrj.br>
2020-01-05 12:57:12 -0300

    Gerar senha usando ENTER

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
+let input = document.getElementById("tamanho")
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault()
+   document.getElementById("myBtn").click()
+  }
+})
+
 function numeroAleatorio(min, max) { // Intervalo [min,max]
-  const randomBuffer = new Uint32Array(1);
+  const randomBuffer = new Uint32Array(1)
 
-  window.crypto.getRandomValues(randomBuffer);
+  window.crypto.getRandomValues(randomBuffer)
 
-  let randomNumber = randomBuffer[0] / (0xffffffff + 1);
+  let randomNumber = randomBuffer[0] / (0xffffffff + 1)
 
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(randomNumber * (max - min + 1)) + min; 
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(randomNumber * (max - min + 1)) + min
   // return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
